829c47388065eaf72dd065d7271d6866a55dd095
roma1909 <rahma.abulfatouh1@gmail.com>
2025-02-13 11:25:42 +0200

    LINQ - Element Operators

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using static assignment.ListGenerators;
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using static assignment.ListGenerators;
 namespace assignment
 {
 
@@ -42,6 +43,38 @@ namespace assignment
             //    Console.WriteLine(item);
             //}
 
+
+            #endregion
+
+
+            #endregion
+
+            #region LINQ - Element Operators
+
+            #region 1. Get first Product out of Stock 
+
+            //var res = ProductList.FirstOrDefault();
+            //Console.WriteLine(res);
+            #endregion
+
+            #region 2. Return the first product whose Price > 1000, unless there is no match, in which case null is returned.
+
+            //var res = ProductList.Where(p => p.UnitPrice > 1000).FirstOrDefault(new Product
+            //{
+            //    ProductName="there is no products matched"
+            //});
+
+            //Console.WriteLine(res);
+
+            #endregion
+
+            #region 3.Retrieve the second number greater than 5
+
+            //int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+
+            //var res = Arr.Where(n => n > 5).ElementAt(3);
+            //Console.WriteLine(res);//7
 
             #endregion
 
